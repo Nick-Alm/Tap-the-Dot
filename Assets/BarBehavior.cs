920a670dd69170853fcf46b1c947ceb4e7972889
Nick Alm <alm.nicholas.2013@gmail.com>
2019-01-23 16:24:51 -0700

    add todos and comments to return to when needed

--- Assets/BarBehavior.cs
+++ Assets/BarBehavior.cs
@@ -4,17 +4,25 @@ using UnityEngine;
 
 public class BarBehavior : MonoBehaviour {
 
-	public float speed;
-	public Rigidbody rb;
-	public GameObject Dot;
-	public Rigidbody dotRB;
+	public float speed; // we will need to set this upon each level depending on the difficulty settings
+	public Rigidbody rb; // Rigidbody for the bar
+	public GameObject Dot; // Dot... Will likely need to move when refactoring
+	public Rigidbody dotRB; // rigidbody for the dot... again, will likely need to move
+
+	// TODOS
+	// [] Get locations of objects
+	// [] Move bar based on location of the dot
+	// 	-	[] Allow for bar's movement to proceed after passing by the bar
+	// [] Get some sort of user input to stop movement -- Bool based on input
+	// [] Define a success in terms of the bar's stopping in relation to the dot
+	// [] Define movement in regard to angle
 	
 	// Use this for initialization
 	void Start () {
-		rb = GetComponent<Rigidbody>();
-		dotRB = Dot.GetComponent<Rigidbody>();
-		dotRB.useGravity = false;
-		rb.useGravity = false;
+		rb = GetComponent<Rigidbody>(); // find bar's rigidbody
+		dotRB = Dot.GetComponent<Rigidbody>(); // find dot's rigidbody
+		dotRB.useGravity = false; // make sure this doesn't fall
+		rb.useGravity = false; // also make sure this doesn't fall... will define movement behavior later using the speed based upon the dot's location
 	}
 	
 	// Update is called once per frame
