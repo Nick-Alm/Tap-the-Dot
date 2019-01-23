95bf3ba0fb1d6bb21f162406e5366cb1698fd626
Nick Alm <alm.nicholas.2013@gmail.com>
2019-01-23 16:08:23 -0700

    First Commit - Set up project files

---
+++ Assets/BarBehavior.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarBehavior : MonoBehaviour {
+
+	public float speed;
+	public Rigidbody rb;
+	public GameObject Dot;
+	public Rigidbody dotRB;
+	
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		dotRB = Dot.GetComponent<Rigidbody>();
+		dotRB.useGravity = false;
+		rb.useGravity = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
