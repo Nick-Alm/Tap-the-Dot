70b64b70ded62ec83e3e7945022d06b1117d4c61
Nick Alm <alm.nicholas.2013@gmail.com>
2019-02-03 12:28:29 -0700

    update files for first milestone

--- Assets/Scripts/BarBehavior.cs
+++ Assets/Scripts/BarBehavior.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BarBehavior : MonoBehaviour {
 
@@ -8,6 +9,11 @@ public class BarBehavior : MonoBehaviour {
 	public Rigidbody rb; // Rigidbody for the bar
 	public GameObject Dot; // Dot... Will likely need to move when refactoring
 	public Rigidbody dotRB; // rigidbody for the dot... again, will likely need to move
+	public Vector3 myVector;
+	public bool userClicked, hasIntersected;
+	public GameObject successMessage;
+    Collider m_Collider, m_Collider2;
+	public Text text;
 
 	// TODOS
 	// [] Get locations of objects
@@ -19,14 +25,49 @@ public class BarBehavior : MonoBehaviour {
 	
 	// Use this for initialization
 	void Start () {
+		userClicked = false;
+		hasIntersected = false;
+		myVector = new Vector3(0.0f, -0.01f, 0.0f);
 		rb = GetComponent<Rigidbody>(); // find bar's rigidbody
 		dotRB = Dot.GetComponent<Rigidbody>(); // find dot's rigidbody
 		dotRB.useGravity = false; // make sure this doesn't fall
 		rb.useGravity = false; // also make sure this doesn't fall... will define movement behavior later using the speed based upon the dot's location
+		if (Dot != null)
+            m_Collider = Dot.GetComponent<Collider>();
+
+        //Check that the second GameObject exists in the Inspector and fetch the Collider
+        m_Collider2 = GetComponent<Collider>();
+		text = successMessage.GetComponent<Text>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (Input.GetButton("Fire1"))
+        {
+			userClicked = true;
+        }
+		if (!userClicked){
+			moveBar();
+		}
+		if (m_Collider.bounds.Intersects(m_Collider2.bounds))
+        {
+			hasIntersected = true;
+            Debug.Log("Bounds intersecting");
+			//text.text = "Text changed!";
+        }
+		else{
+			hasIntersected = false;
+		}
+		if(userClicked && hasIntersected){
+			text.text = "Success!";
+		}
+		if(userClicked && !hasIntersected){
+			text.text = "You missed!";
+		}
 		
+	}
+
+	void moveBar(){
+		transform.Translate(myVector);
 	}
 }
---
+++ Assets/Scripts/CheckCollisions.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CheckCollisions : MonoBehaviour
+{
+    public GameObject dot, bar, successMessage;
+    Collider m_Collider, m_Collider2;
+	public Text text;
+
+    void Start()
+    {
+        //Check that the first GameObject exists in the Inspector and fetch the Collider
+        if (dot != null)
+            m_Collider = dot.GetComponent<Collider>();
+
+        //Check that the second GameObject exists in the Inspector and fetch the Collider
+        if (bar != null)
+            m_Collider2 = bar.GetComponent<Collider>();
+		text = successMessage.GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        //If the first GameObject's Bounds enters the second GameObject's Bounds, output the message
+        if (m_Collider.bounds.Intersects(m_Collider2.bounds))
+        {
+            Debug.Log("Bounds intersecting");
+			text.text = "Text changed!";
+        }
+    }
+}
