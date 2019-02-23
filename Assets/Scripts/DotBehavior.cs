67b64cb8b004d26f2833be2cd4b00e4d84a485d3
Nick Alm <alm.nicholas.2013@gmail.com>
2019-02-23 13:45:59 -0700

    import random to make dot programmatically appear random

--- Assets/Scripts/DotBehavior.cs
+++ Assets/Scripts/DotBehavior.cs
@@ -1,13 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class DotBehavior : MonoBehaviour {
 
+	private Vector3 spherePosition;
+
 	// Use this for initialization
 	void Start () {
-
+		spherePosition = new Vector3(getRandomX(), getRandomY(), 5.5f);
+		transform.position = spherePosition;
 		
+	}
+
+	private float getRandomX(){
+		double minimum = -11.5f;
+		double maximum = minimum * -1.0;
+		System.Random random = new System.Random();
+    	return (float)(random.NextDouble() * (maximum - minimum) + minimum);
+		//return 2.0f;
+	}
+
+	private float getRandomY(){
+		double minimum = -4.5f;
+		double maximum = minimum * -1.0;
+		System.Random random = new System.Random();
+    	return (float)(random.NextDouble() * (maximum - minimum) + minimum);
+		//return 2.0f;
 	}
 	
 	// Update is called once per frame
